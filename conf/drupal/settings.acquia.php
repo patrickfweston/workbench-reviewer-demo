e676a4f7821abec26140c866be9f5d7c5133eebe
Patrick Weston <patrickfweston@gmail.com>
2017-12-01 01:57:41 +0000

    Update password

--- conf/drupal/settings.acquia.php
+++ conf/drupal/settings.acquia.php
@@ -38,7 +38,7 @@ $cli = (php_sapi_name() == 'cli');
 // PASSWORD-PROTECT NON-PRODUCTION SITES (i.e. staging/dev)
 if (!$cli && (isset($_ENV['AH_NON_PRODUCTION']) && $_ENV['AH_NON_PRODUCTION'])) {
   $username = 'palantir';
-  $password = 'some mediocre password';
+  $password = 'workbench-reviewer';
   if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
     header('WWW-Authenticate: Basic realm="This site is protected"');
     header('HTTP/1.0 401 Unauthorized');
