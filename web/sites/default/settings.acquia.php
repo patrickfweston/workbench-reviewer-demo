26eba508d233e100fe5fa32a15b7edb0935a68bb
Patrick Weston <patrickfweston@gmail.com>
2017-12-01 05:21:27 +0000

    Add settings.php files

---
+++ web/sites/default/settings.acquia.php
@@ -0,0 +1,30 @@
+<?php
+
+// Configure the public, private, and tmp files directories.
+$settings['file_public_path'] = 'sites/default/files';
+$settings['file_private_path'] = "/mnt/files/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/files-private";
+$config['system.file']['path']['temporary'] = "/mnt/gfs/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/tmp";
+
+// Host URL patterns for Acquia.
+$settings['trusted_host_patterns'] = [
+  '^workbenchreviewerkgvspucxrx.devcloud.acquia-sites.com',
+  '^workbenchreviewerwj3iaaszrt.devcloud.acquia-sites.com',
+];
+
+// Include the Acquia database connection and other config.
+if (file_exists('/var/www/site-php')) {
+  require '/var/www/site-php/workbenchreviewer/workbenchreviewer-settings.inc';
+}
+
+// Enable memcache on Acquia.
+if (file_exists('/var/www/site-php')) {
+  // Memcache settings.
+  $settings['cache']['default'] = 'cache.backend.memcache';
+}
+
+//// Add an htaccess prompt on dev.
+//// @see https://docs.acquia.com/articles/password-protect-your-non-production-environments-acquia-hosting#phpfpm
+
+// Make sure Drush keeps working.
+// Modified from function drush_verify_cli()
+$cli = (php_sapi_name() == 'cli');
---
+++ web/sites/default/settings.build.php
@@ -0,0 +1,28 @@
+<?php
+
+$databases = array();
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'drupal',
+  'username' => 'root',
+  'password' => 'root',
+  'host' => '127.0.0.1',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
+
+$config_directories = array();
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../conf/drupal/config';
+
+$settings['hash_salt'] = '4ec8b7160d8b7ac8f9b581686c651d73c2e46640a118e1a4ddf1739f0542bb6d';
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+$settings['file_public_path'] = 'sites/default/files';
+$settings['file_private_path'] = '';
+
+$config['acquia_connector.settings']['hide_signup_messages'] = TRUE;
+
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
