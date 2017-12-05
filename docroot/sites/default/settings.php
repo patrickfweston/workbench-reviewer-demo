a3e06b88a9461e85c1f33ca050fc8f1560c69a1a
Patrick Weston <patrickfweston@gmail.com>
2017-12-05 11:30:35 +0000

    Set config directory after Acquia inclusion

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -2,12 +2,6 @@
 
 $databases = array();
 
-$settings['install_profile'] = 'config_installer';
-
-$config_directories = array();
-// In this codebase, config is managed by git and lives outside of the Drupal root.
-$config_directories = [CONFIG_SYNC_DIRECTORY => '../conf/drupal/config'];
-
 $settings['hash_salt'] = '4ec8b7160d8b7ac8f9b581686c651d73c2e46640a118e1a4ddf1739f0542bb6d';
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
@@ -21,6 +15,12 @@ $config['acquia_connector.settings']['hide_signup_messages'] = TRUE;
 if (file_exists('/var/www/site-php')) {
   include $app_root . '/' . $site_path . '/settings.acquia.php';
 }
+
+$settings['install_profile'] = 'config_installer';
+
+$config_directories = array();
+// In this codebase, config is managed by git and lives outside of the Drupal root.
+$config_directories = [CONFIG_SYNC_DIRECTORY => '../conf/drupal/config'];
 
 // Dev environment settings file provided by the-build.
 if (file_exists($app_root . '/' . $site_path . '/settings.build.php')) {
